8b7144f9c6ea4c91ea2ab9b04171d8d0f07235b2
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-13 10:10:22 +0100

    Ho messo laconversione di un numero in ASCII

--- solBase64/Program.cs
+++ solBase64/Program.cs
@@ -12,11 +12,28 @@ namespace solBase64
         {
             string plainText = " ";
             string base64EncodedData;
+            int n;
+
+            Console.WriteLine("Inserisci unnumero");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            string ascii = System.Text.ASCIIEncoding.ASCII.GetString(new byte[] { (byte)n });
+
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+
+            Console.WriteLine(ascii);
+
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
 
             Console.WriteLine("Inserisci una parola");
             plainText = Console.ReadLine();
 
             byte[] plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
 
             for (int i = 0; i < plainTextBytes.Length; i++)
             {
@@ -25,7 +42,7 @@ namespace solBase64
             }
             
             base64EncodedData = Convert.ToBase64String(plainTextBytes);
-            Console.WriteLine(base64EncodedData);
+            Console.WriteLine("La codifica in base 64 della parola '" + plainText + "' è " + base64EncodedData);
 
             byte[] base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
             plainText = System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
